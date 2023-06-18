307fd8945a982cb9ad7e44261a17d0cb03850bfc
anilac13 <anil4u2496@gmail.com>
2023-06-18 08:12:03 +0530

    [Anil] Add:Add multiple employees to payroll

---
+++ MultiThreading/PayrollOperations.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiThreading
+{
+    public class PayrollOperations
+    {
+        public List<Payroll> payrolls1 = new List<Payroll>();
+        public void AddEmployee(List<Payroll> payrolls1)
+        {
+            /*foreach (Payroll payroll in payrolls1)
+            {
+                Console.WriteLine($"Employee being added:- {payroll.EmployeeName}");
+                this.AddEmpoyeesToList(payroll);
+                Console.WriteLine($"Employee added: {payroll.EmployeeName}");
+            }*/
+
+            payrolls1.ForEach(payrollData =>
+            {
+                Console.WriteLine($"Employee being added:- {payrollData.EmployeeName}");
+                this.AddEmpoyeesToList(payrollData);
+                Console.WriteLine($"Employee added: {payrollData.EmployeeName}");
+            });
+        }
+        public void AddEmpoyeesToList(Payroll pay)
+        {
+            payrolls1.Add(pay);
+        }
+    }
+}
--- MultiThreadingTest/UnitTest1.cs
+++ MultiThreadingTest/UnitTest1.cs
@@ -1,3 +1,5 @@
+using MultiThreading;
+
 namespace MultiThreadingTest
 {
     [TestClass]
@@ -6,6 +8,18 @@ namespace MultiThreadingTest
         [TestMethod]
         public void TestMethod1()
         {
+            List<Payroll> payrolls = new List<Payroll>();
+            payrolls.Add(new Payroll(employeeId: 1, employeeName: "Bruce", employeeAge: 20, salary: 18000.0));
+            payrolls.Add(new Payroll(employeeId: 2, employeeName: "Ankit", employeeAge: 22, salary: 25000.0));
+            payrolls.Add(new Payroll(employeeId: 3, employeeName: "Ram", employeeAge: 24, salary: 20000.0));
+            payrolls.Add(new Payroll(employeeId: 4, employeeName: "Maria", employeeAge: 25, salary: 23000.0));
+            payrolls.Add(new Payroll(employeeId: 5, employeeName: "Rashmi", employeeAge: 23, salary: 22000.0));
+            
+            PayrollOperations operations = new PayrollOperations();
+            DateTime startDateTime = DateTime.Now;
+            operations.AddEmployee(payrolls);
+            DateTime stopDateTime = DateTime.Now;
+            Console.WriteLine($"Time with Thread:- {stopDateTime - startDateTime}");
         }
     }
 }
