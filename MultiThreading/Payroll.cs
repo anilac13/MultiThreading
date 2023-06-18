021505bb0cdd0257e333e8ee015dc80bb8a9d240
anilac13 <anil4u2496@gmail.com>
2023-06-18 07:18:30 +0530

    [Anil] Add:Add multiple employees to payroll

---
+++ MultiThreading/Payroll.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiThreading
+{
+    public class Payroll
+    {
+        public int EmployeeId { get; set; }
+        public string EmployeeName { get; set; }
+        public int EmployeeAge { get; set; }
+        public double Salary { get; set; }
+        public Payroll(int employeeId, string employeeName, int employeeAge, double salary)
+        {
+            this.EmployeeId = employeeId;
+            this.EmployeeName = employeeName;
+            this.EmployeeAge = employeeAge;
+            this.Salary = salary;
+        }
+    }
+}
